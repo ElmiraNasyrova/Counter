8c3ec54f1929461275d39e6ffc98767ea85342b8
Elmira <elm130892@gmail.com>
2024-07-22 23:34:52 +0300

    Initial Commit

---
+++ counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  counter
+//
+//  Created by Эльмира Насырова on 22.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
